42ea54ddf6cbe5aeeba718bc402134b144468b76
Quang Nghĩa <tonystrinh@gmail.com>
2019-11-05 14:07:59 +0700

    prepare config to up host

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-export default {
-    'client-domain': '//localhost:6969/',
-    'server-domain': '//localhost:3000/'
-}
-
 // export default {
-//     'client-domain': 'https://btgk-1612422.herokuapp.com/',
-//     'server-domain': 'https://btcn06-1612422.herokuapp.com/'
+//     'client-domain': '//localhost:6969/',
+//     'server-domain': '//localhost:3000/'
 // }
+
+export default {
+    'client-domain': 'https://btgk-1612422.herokuapp.com/',
+    'server-domain': 'https://btcn06-1612422.herokuapp.com/'
+}
