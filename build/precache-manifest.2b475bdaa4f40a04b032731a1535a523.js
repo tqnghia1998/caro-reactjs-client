a67d6c599f2530890081433c32892172ae386be2
Quang Nghĩa <tonystrinh@gmail.com>
2019-11-05 21:43:45 +0700

    fix bug again

---
+++ build/precache-manifest.2b475bdaa4f40a04b032731a1535a523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73431f69633ed31a77371cfad360f98e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c51fe4f19903f680af2f",
+    "url": "/static/css/main.2728a5ca.chunk.css"
+  },
+  {
+    "revision": "1ccd6b7d256990a73cc4",
+    "url": "/static/js/2.c90cb2b4.chunk.js"
+  },
+  {
+    "revision": "c51fe4f19903f680af2f",
+    "url": "/static/js/main.2635f583.chunk.js"
+  },
+  {
+    "revision": "5237db175a82411b449d",
+    "url": "/static/js/runtime~main.d4a129de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
