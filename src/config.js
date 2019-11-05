022d84a43674cfabcac70c0aa253de968d677348
Quang Nghĩa <tonystrinh@gmail.com>
2019-11-05 22:22:53 +0700

    edit config file

--- src/config.js
+++ src/config.js
@@ -1,9 +1,13 @@
-// export default {
-//     'client-domain': '//localhost:6969/',
-//     'server-domain': '//localhost:3000/'
-// }
+// These exports are for localhost
+var exports_1 = {
+    'client-domain': '//localhost:6969/',
+    'server-domain': '//localhost:3000/'
+};
 
-export default {
+// These exports are for uploading to heroku
+var exports_2 = {
     'client-domain': 'https://btgk-1612422.herokuapp.com/',
     'server-domain': 'https://btcn06-1612422.herokuapp.com/'
-}
+};
+
+export default exports_1;
